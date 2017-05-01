1d82afdacaae4ce64a3d02eb07da8ccc5eb966ac
AmFearLiath <amfearliath@gmx.de>
2017-05-01 23:52:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ Changelog
 
 1.3.2 - Added optional search for duplicates in descriptions. Searchalgorythm improved. Configuration page changed.
 
-1.3.3 – Global var changed, to prevent error messages
+1.3.3 â€“ Global var changed, to prevent error messages
 
 1.3.4 - Stopwords now shown, if ad was blocked for this reason, Search for duplicates improved, translations corrected
 
@@ -67,23 +67,27 @@ if (Params::getParam('spamcomment') == 'activate') {
 }
 
 // Login Check
-$action = Params::getParam('action');
-$email = Params::getParam('email');
-$password = Params::getParam('password', false, false);
-
-if ($action == 'login_post' && !empty($email) && !empty($password)) {
+if ($sp->_get('sp_security_activate') == '1') {
     
-    $logins = $sp->_countUserLogin($email);
-    //$logins = 5;
-    if (!empty($logins) && $logins >= 5) {
-        die("Contact support if you have errors while login");
-    }
-    if (!$sp->_checkUserLogin($email, $password)) {
-        $sp->_countUserLogin($email);
-    } else {
-        $sp->_resetUserLogin($email);    
-    }
-}                                       
+    $action = Params::getParam('action');
+    $email = Params::getParam('email');
+    $password = Params::getParam('password', false, false);
+
+    if ($action == 'login_post' && !empty($email) && !empty($password)) {
+        
+        $logins = $sp->_countUserLogin($email);
+        $max_logins = $sp->_get('sp_security_login_count');
+        
+        if (!empty($logins) && $logins >= $max_logins) {
+            $sp->_handleUserLogin($email);
+            return;    
+        } if (!$sp->_checkUserLogin($email, $password)) {
+            $sp->_increaseUserLogin($email);
+        } else {
+            $sp->_resetUserLogin($email);    
+        }
+    }                                       
+}
 
 // User wants to delete his contact mail
 $delete_mail = Params::getParam('delete_contact_mail');
